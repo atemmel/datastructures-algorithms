f215a7b38347ce2dba91415d15635599ef89a78f
atemmel <temmeladam@gmail.com>
2019-04-29 23:07:08 +0200

    Visar nu fittad graf

--- lab2/plot.py
+++ lab2/plot.py
@@ -1,21 +1,23 @@
 #!/bin/python
 import matplotlib
 import matplotlib.pyplot as plt
-import numpy as np
 import scipy.optimize as opt
+import numpy as np
 
 matplotlib.use('Qt5Agg')
 
 def func(x, a, b, c):
      return x ** 2 * a + b * x + c
 
-data = np.genfromtxt("insertion_random_old.csv", delimiter=',')[:,:-1]
+data = np.genfromtxt("insertion_random.csv", delimiter=',')[:,:-1]
 x = data[0]
 y = data[1:]
 
-optimizedParameters, pcov = opt.curve_fit(func, x, y[0], p0=[0.1,0,5]);
+print(y.transpose(1,0)[0])
 
-print(optimizedParameters)
+fitData = y.transpose(1,0)[0]
+
+optimizedParameters, pcov = opt.curve_fit(func, x, fitData);
 
 plt.plot(x, y, 'ro', label="data")
 plt.plot(x, func(x, *optimizedParameters), label="fit", color="blue");
